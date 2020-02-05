db2da71c52adef6aeebe0cdd40a2e9db46df5e7a
Sarah Lindau <lsarah@student.chalmers.se>
2020-02-05 11:32:14 +0100

    Add predict funtion

--- AITools/KNNClassifier.py
+++ AITools/KNNClassifier.py
@@ -1,12 +1,33 @@
+from Utils.py import get_euclidan_distances
+
 class KNNClassifier:
-    def __init__(self):
-        pass
 
-    def fit(self):
-        pass
+    def __init__(self, K):
+        self.data = None
+        self.labels = None
+        self.K = K
 
-    def predict(self):
-        pass
+    def fit(self, X,Y):
+        self.data = X
+        self.labels = Y
+
+    def predict(self, X):
+        for _,row in X.iterrows():
+           self.predict_label(row)
+
+    def predict_label(self,row):
+        distances = get_euclidan_distances(self.data,row)
+        distances = sorted(distances, key=distances.get)
+        indexes = distances[:self.K]
+        return self.evaluate_most_common_label(indexes)
+
+    def evaluate_most_common_label(self, indexes):
+        count_ones = sum(self.labels[i] for i in indexes)
+        if count_ones > self.K/2:
+            return 1
+        else:
+            return 0
+
 
     def score(self):
         pass
--- AITools/Utils.py
+++ AITools/Utils.py
@@ -10,8 +10,8 @@ def calculate_euclidian_distance(row1, row2):
 
 def get_euclidan_distances(df, newrow):
 
-    distances = []
-    for _,row in df.iterrows():
-        distances.append(calculate_euclidian_distance(row,newrow))
+    distances = {}
+    for index,row in df.iterrows():
+        distances[index] = calculate_euclidian_distance(row,newrow)
 
     return distances
