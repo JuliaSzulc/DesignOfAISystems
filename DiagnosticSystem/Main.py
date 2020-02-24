f95ed9b218c0c6b48b84551cad233f5d9cc9ecbb
Julia Szulc <julszulc@gmail.com>
2020-02-24 20:12:08 +0100

    finished

--- DiagnosticSystem/Main.py
+++ DiagnosticSystem/Main.py
@@ -3,14 +3,27 @@ import pandas as pd
 import RandomForestClassifier
 import SVM
 import RuleBased
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_density(param):
+    x_b = [data_df[param][i] for i in data_df.index if not data_df['malignant'][i]]
+    x_m = [data_df[param][i] for i in data_df.index if data_df['malignant'][i]]
+
+    sns.kdeplot(x_b, color='b', bw=0.01).set_title(param)
+    sns.kdeplot(x_m, color='r', bw=0.01)
+    plt.show()
 
 with open("data/wdbc.pkl", 'rb') as f:
     data = pickle.load(f)
 data_df = pd.DataFrame(data)
 
+# for key in data_df.keys():
+#     plot_density(key)
+
 print('Random forest')
-#RandomForestClassifier.classify(data_df)
+RandomForestClassifier.classify(data_df)
 print('SVC')
-#SVM.classify(data_df)
+SVM.classify(data_df)
 print('Rule based')
 RuleBased.classify(data_df)
--- DiagnosticSystem/RuleBased.py
+++ DiagnosticSystem/RuleBased.py
@@ -4,32 +4,40 @@ from statistics import mean, stdev
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-nuclei_index = ['_0' ,'_1','_2']
-params = ['radius', 'area', 'smoothness', 'concavity', 'compactness', 'concave points', 'symmetry', 'fractal dimension']
+nuclei_index = ['_0', '_1', '_2']
+params = ['perimeter', 'area', 'concave points',
+          'concavity', 'radius', 'compactness']
 stdev_params = ['texture']
 thresholds = {}
 
+
 def define_rule(param):
-    return lambda x: x[param] >= thresholds[param], 1
+    return lambda x: x[param] >= thresholds[param]
+
 
 def define_stdev_rule(param):
-    return lambda x: x[param] >= thresholds[param], 1
+    return lambda x: x[param] >= thresholds[param]
+
 
 def get_threshold(param_name, data):
-    malignant_average = mean([x[param_name+i] for i in nuclei_index for _,x in data.iterrows() if x['malignant']])
-    benign_average = mean([x[param_name+i] for i in nuclei_index for _,x in data.iterrows() if not x['malignant']])
-    return (malignant_average+benign_average)/2
+    malignant_average = mean(
+        [x[param_name] for _, x in data.iterrows() if x['malignant']])
+    benign_average = mean(
+        [x[param_name] for _, x in data.iterrows() if not x['malignant']])
+    return (malignant_average + benign_average) / 2
 
-def get_stdev_threshold(param_name,data):
+
+def get_stdev_threshold(param_name, data):
     mal_stdevs = []
     ben_stdevs = []
-    for _,row in data.iterrows():
-        nuclei_values = [row[param_name+i] for i in nuclei_index]
+    for _, row in data.iterrows():
+        nuclei_values = [row[param_name + i] for i in nuclei_index]
         if row['malignant']:
             mal_stdevs.append(stdev(nuclei_values))
         else:
             ben_stdevs.append(stdev(nuclei_values))
-    return (mean(mal_stdevs)+mean(ben_stdevs))/2
+    return (mean(mal_stdevs) + mean(ben_stdevs)) / 2
+
 
 def predict(X):
     Y_pred = []
@@ -38,36 +46,34 @@ def predict(X):
     return Y_pred
 
 
-def fit(X,Y):
+def fit(X, Y):
     df = X.copy()
     df['malignant'] = Y
     for param in params:
-        thresholds[param] = get_threshold(param,df)
-    for param in stdev_params:
-        thresholds[param] = get_stdev_threshold(param,df)
+        for i in nuclei_index:
+            param_name = param + i
+            thresholds[param_name] = get_threshold(param_name, df)
+    # for param in stdev_params:
+    #     thresholds[param] = get_stdev_threshold(param, df)
 
 
 def predict_single_entry(x):
     for param in params:
-        if define_rule(param):
-            return 1
-        for stdev_param in stdev_params:
-            if define_stdev_rule(stdev_param):
+        for i in nuclei_index:
+            if define_rule(param + i)(x):
                 return 1
+    # for stdev_param in stdev_params:
+    #     if define_stdev_rule(stdev_param)(x):
+    #         return 1
     return 0
-
 
 
 def classify(df):
     Y = df['malignant']
-    X = df.drop('malignant',1)
+    X = df.drop('malignant', 1)
 
-    X_train, X_test, Y_train,Y_test = train_test_split(X,Y, test_size=0.2)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
     fit(X_train, Y_train)
     Y_pred = predict(X_test)
     print(accuracy_score(Y_test, Y_pred))
-
-
-
-
