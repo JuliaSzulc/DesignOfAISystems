2836c07523dd96b46024fbef6644c27544641c2a
Sarah Lindau <lsarah@student.chalmers.se>
2020-02-27 10:41:49 +0100

    simulation work

--- GamePlayingSystem/Tree.py
+++ GamePlayingSystem/Tree.py
@@ -13,29 +13,57 @@ class Tree:
         return exp_node
 
     def expand(self, expandable_node):
-        return self.create_child(expandable_node)
+        child = self.expand_child(expandable_node)
+        expandable_node.children.append(child)
+        return child
 
-    def simulate(self, policy, node):
+
+    def simulate(self, node):
         while not node.is_terminal():
+            node = self.expand_child(node)
+        return node
 
 
-    def evaluate(self, whole_path):
+    def evaluate(self, node):
+        self.check_winner(node)
         result = 1 #-1 loss,0 draw,1 win
         return result
+
+    def check_winner(self, node):
+        state = node.state
+        for i in range(state.shape[1]):
+            char = state[0][i]
+            if all(state[j][i]==char for j in range(1,state.shape[0])):
+                return char
+
+        for j in range(state.shape[0]):
+            char = state[j][0]
+            if all(state[j][i] == char for i in range(1, state.shape[1])):
+                return char
+
+            char = state[0][0]
+            if all(state[i][i] == char for i in range(1, state.shape[0])):
+                return char
+
+            char = state[0][-1]
+            if all(state[i][-i] == char for i in range(1, state.shape[0])):
+                return char
+        return None
+
+
 
     def get_random_path(self):
         nodes_copy = self.root.copy()
         #Get children all the way
         #When there is no child, create a new node unless it's terminal - then we need to select different path
 
-    def create_child(self, node):
+
+    def expand_child(self, node):
         child_state = node.state
-        empty_fields = np.where(child_state== b'')
+        empty_fields = np.where(child_state == b'')
         chosen_index = choice(range(len(empty_fields[0])))
         square = (empty_fields[0][chosen_index], empty_fields[1][chosen_index])
         child_turn = 'o' if node.turn == 'x' else 'x'
         child_state[square[0], square[1]] = child_turn
-        child = Node(child_state, node.empty_squares-1, child_turn)
-        node.children.append(child)
+        child = Node(child_state, node.empty_squares - 1, child_turn)
         return child
-
