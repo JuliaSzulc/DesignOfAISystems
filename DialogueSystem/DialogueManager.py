4ab70795d8f5c74c34a1f7ed6bba8f517e2c60d1
Sarah Lindau <lsarah@student.chalmers.se>
2020-03-04 14:24:53 +0100

    framework finish

--- DialogueSystem/DialogueManager.py
+++ DialogueSystem/DialogueManager.py
@@ -1,5 +1,6 @@
 from FormBuilder import *
 from Task import *
+from TaskManager import *
 
 
 class DialogueManager:
@@ -7,17 +8,31 @@ class DialogueManager:
     def __init__(self, task_file_name):
         self.tasks = task_file_name
         self.form_builder = FormBuilder()
+        self.task_manager = TaskManager()
 
-    def initialize_conversation(self):
+    def talk(self):
+
+        print("Hi, I am your digital Västtrafik assistant, Vivvi.")
         while True:
-            user_input = input("Hi, I am your digital Västtrafik assistant Vivvi, what can I help you with?\n")
+            user_input = input("How can I help you today?\n")
             task = self.determine_task(user_input)
 
             if task:
-                form_builder.create_form(task, user_input)
+                form = self.process_input(task, user_input)
+                output = task_manager.get_output(form)
+                print(output)
 
 
-        #Process
+    def process_input(self, task, user_input):
+        form, more_info = self.form_builder.create_form(task, user_input)
+
+        while more_info:
+            missing_field = more_info[0]
+            user_input = input("Please specify your {}.".format(task.form_field[missing_field]))
+            form, more_info = self.form_builder.create_form(task, user_input, form)
+
+        return form
+
 
     def determine_task(self, user_input):
         for task in self.tasks:
---
+++ DialogueSystem/TaskManager.py
@@ -0,0 +1,8 @@
+
+class TaskManager:
+
+    def create_query(self, form):
+        pass
+
+    def get_output(self, form):
+        return self.create_query(form)
