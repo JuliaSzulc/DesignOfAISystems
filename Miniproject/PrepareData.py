d049c099d7e9aed0f23e7d5eee12520d57e287bc
Sarah Lindau <lsarah@student.chalmers.se>
2020-03-16 18:22:55 +0100

    Fixed tags

--- Miniproject/PrepareData.py
+++ Miniproject/PrepareData.py
@@ -1,17 +1,42 @@
 import pandas as pd
+from collections import Counter
+
+def prepare_data(path="stackoverflow.csv"):
+    df = pd.read_csv(path, encoding="ISO-8859-1")
+    df.drop(columns=['Id', 'Unnamed: 0','OwnerUserId', 'CreationDate', 'ClosedDate', 'Body', 'Score'], inplace=True)
+    df = prepare_tags(df)
 
 
-df = pd.read_csv("stacksample/Questions.csv", encoding="ISO-8859-1")
-
-tags = pd.read_csv("stacksample/Tags.csv", encoding="ISO-8859-1", dtype={'Tag': str})
-tags['Tag'] = tags['Tag'].astype(str)
-grouped_tags = tags.groupby("Id")['Tag'].apply(lambda tags: ' '.join(tags))
-grouped_tags.reset_index()
+    #Split data in to testing and training (vill ha med all tags någon gång i training)
 
 
-grouped_tags_final = pd.DataFrame({'Id':grouped_tags.index, 'Tags':grouped_tags.values})
+def prepare_tags(df):
+    df['Tags'] = df['Tags'].apply(lambda question_tags: question_tags.split())
+    tags = get_unique_tags(df)
+    df['Tags'] = df['Tags'].apply(lambda question_tags: filter_unique_tags(question_tags, tags))
+    df['Tags'] = df['Tags'].apply(lambda question_tags: None if len(question_tags) == 0 else question_tags)
+    df.dropna(inplace=True)
+    return df
 
-df = df.merge(grouped_tags_final, on='Id')
+def get_unique_tags(df):
+    all_tags = [tag for tags in df['Tags'].values for tag in tags]
+    tags = filter_tags(all_tags)
+    return tags
+
+def filter_tags(all_tags):
+    filtered_tags = []
+    tags_counter = Counter(all_tags)
+    for tag in tags_counter.keys():
+        if tags_counter[tag] > 1:
+            filtered_tags.append(tag)
+    return filtered_tags
+
+def filter_unique_tags(tags, unique_tags):
+    filtered_tags = []
+    for tag in tags:
+        if tag in unique_tags:
+            filtered_tags.append(tag)
+    return filtered_tags
 
 
-df.head(10000).to_csv("stackoverflow.csv")
+prepare_data()
